bcda391296b8ade76525a2da56608896fbde5f8a
fikante <154019349+fikante@users.noreply.github.com>
2024-07-22 22:34:53 +0300

    Time: 44 ms (22.47%) | Memory: 16.6 MB (37.44%) - LeetSync

---
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.py
@@ -0,0 +1,10 @@
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        num1 = []
+        num2 = []
+        for i in range(1,n+1):
+            if i % m == 0:
+                num2.append(i)
+            else:
+                num1.append(i)
+        return sum(num1) - sum(num2)
