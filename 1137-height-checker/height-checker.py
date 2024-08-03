341abe8f9a6a4ba058fcf668a4947d066da196f0
fikante <154019349+fikante@users.noreply.github.com>
2024-08-04 01:30:05 +0300

    Time: 42 ms (36.21%) | Memory: 16.3 MB (88.94%) - LeetSync

--- 1137-height-checker/height-checker.py
+++ 1137-height-checker/height-checker.py
@@ -2,9 +2,11 @@ class Solution:
     def heightChecker(self, heights: List[int]) -> int:
         sorted_heights = sorted(heights)
         h = 0
+        e = 0
         count = 0
         while h < len(heights):
-            if heights[h] != sorted_heights[h]:
+            if heights[h] != sorted_heights[e]:
                 count += 1
             h += 1
+            e += 1
         return count
