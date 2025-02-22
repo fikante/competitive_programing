4448be906fcd6724984aba3b11e7f40d9be23114
fikante <154019349+fikante@users.noreply.github.com>
2025-02-22 09:32:30 +0300

    Time: 47 ms (69.52%) | Memory: 23.3 MB (21.88%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.py
+++ 917-boats-to-save-people/boats-to-save-people.py
@@ -5,15 +5,8 @@ class Solution:
         count = 0
         people.sort()
         while l <= r:
-            if l == r:
-                count += 1
+            if people[l] + people[r] <= limit:
                 l += 1
-                r -= 1
-            elif people[l] + people[r] <= limit:
-                count += 1
-                l += 1
-                r -= 1
-            else:
-                count += 1
-                r -= 1
+            count += 1
+            r -= 1
         return count
