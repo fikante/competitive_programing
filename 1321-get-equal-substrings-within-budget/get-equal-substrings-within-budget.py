9ba2ebc579baf6c071fda2b23ca5b4a0aedf7eb7
fikante <154019349+fikante@users.noreply.github.com>
2025-02-26 14:23:06 +0300

    Time: 26 ms (76.46%) | Memory: 18 MB (62.59%) - LeetSync

--- 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.py
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.py
@@ -1,13 +1,10 @@
 class Solution:
     def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
-        hashCost = {}
         left, cost, res = 0, 0, 0
         for right in range(len(s)):
-            hashCost[right] = abs(ord(s[right]) - ord(t[right]))
             cost += abs(ord(s[right]) - ord(t[right]))
             while cost > maxCost:
-                cost -= hashCost[left]
-                hashCost.pop(left)
+                cost -= abs(ord(s[left]) - ord(t[left]))
                 left += 1
             res = max(res, right - left + 1)
         return res
