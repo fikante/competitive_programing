e982cea1a91b4332490bddf7445706cea5ba6da7
fikante <154019349+fikante@users.noreply.github.com>
2025-03-04 23:07:38 +0300

    Time: 196 ms (73.06%) | Memory: 33.8 MB (61.62%) - LeetSync

---
+++ 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+class Solution:
+    def shortestSubarray(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        prefix = [0] * (n + 1)
+        for i in range(n):
+            prefix[i + 1] = prefix[i] + nums[i]
+        
+        res = float('inf')
+        dq = deque()
+        
+        for i in range(n + 1):
+            while dq and prefix[i] - prefix[dq[0]] >= k:
+                res = min(res, i - dq.popleft())
+            while dq and prefix[i] <= prefix[dq[-1]]:
+                dq.pop()
+            dq.append(i)
+        
+        return res if res != float('inf') else -1
