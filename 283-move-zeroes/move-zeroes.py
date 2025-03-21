cb31ebdbdf693fd6a734fcff834c6c000858301a
fikante <154019349+fikante@users.noreply.github.com>
2025-03-21 18:40:41 +0300

    Time: 7 ms (46.28%) | Memory: 18.7 MB (94.47%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,60 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        left = 0
-        right = 1
-        while right < len(nums):
-            if nums[left] == 0:
-                if nums[right] == 0:
-                    right += 1
-                else:
-                    nums[left], nums[right] = nums[right], nums[left]
-                    left += 1
-                    right += 1
-            elif nums[left] != 0:
-                left += 1
-                right += 1
-        return nums
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # l, r = 0,0
-        # while r < len(nums):
-        #     if nums[r] != 0:
-        #         nums[l], nums[r] = nums[r], nums[l]
-        #         l += 1
-        #     r += 1
+        l, r = 0,0
+        while r < len(nums):
+            if nums[r] != 0:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+            r += 1
